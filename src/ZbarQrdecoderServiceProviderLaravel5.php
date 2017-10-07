e0773efe0d5af72bff4855961663048e58a76a42
nicard <rra_ncd@hotmail.com>
2017-10-07 10:29:50 -0300

    Update ZbarQrdecoderServiceProviderLaravel5.php
    Correção laravel 5

--- src/ZbarQrdecoderServiceProviderLaravel5.php
+++ src/ZbarQrdecoderServiceProviderLaravel5.php
@@ -6,38 +6,37 @@ use Symfony\Component\Process\ProcessBuilder;
 
 class ZbarQrdecoderServiceProviderLaravel5 extends ServiceProvider {
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-                $this->publishes([
-					__DIR__ . '/../../config/config.php' => config_path('zbar-qrdecoder.php'),
-		]);
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+      $this->publishes([__DIR__ . './config/config.php' => config_path('zbar-qrdecoder.php')]);
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//$this->app['zbardecoder'] = $this->app->share(function($app)
-		$this->app->singleton('zbardecoder' ,function($app)
-		{
-			$processBuilder = new ProcessBuilder();
-			$config = $app['config']->get('zbar-qrdecoder::config');
-			return new ZbarDecoder($config, $processBuilder);
-		});
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //$this->app['zbardecoder'] = $this->app->share(function($app)
+        $this->app->singleton('zbardecoder' ,function($app)
+        {
+            $processBuilder = new ProcessBuilder();
+            $config = $app['config']->get('zbar-qrdecoder');
+            
+            return new ZbarDecoder($config, $processBuilder);
+        });
 
-		$this->app->booting(function()
-		{
-			$loader = AliasLoader::getInstance();
-			$loader->alias('ZbarDecoder', 'RobbieP\ZbarQrdecoder\Facades\ZbarDecoder');
-		});
-	}
+        $this->app->booting(function()
+        {
+            $loader = AliasLoader::getInstance();
+            $loader->alias('ZbarDecoder', 'RobbieP\ZbarQrdecoder\Facades\ZbarDecoder');
+        });
+    }
 
 }
